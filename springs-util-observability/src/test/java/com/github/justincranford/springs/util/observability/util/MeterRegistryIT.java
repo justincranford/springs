40e85ab6d857ccf6f5e51af1e1d729222cedb7bc
Justin Cranford <justincranford@hotmail.com>
2024-08-09 10:31:01 -0400

    Cleanup

--- springs-util-observability/src/test/java/com/github/justincranford/springs/util/observability/util/MeterRegistryIT.java
+++ springs-util-observability/src/test/java/com/github/justincranford/springs/util/observability/util/MeterRegistryIT.java
@@ -3,11 +3,9 @@ package com.github.justincranford.springs.util.observability.util;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.Configuration;
 
 import com.github.justincranford.springs.util.observability.AbstractIT;
 
-@Configuration
 public class MeterRegistryIT extends AbstractIT {
 	@Test
 	void testMeterRegistry() {
