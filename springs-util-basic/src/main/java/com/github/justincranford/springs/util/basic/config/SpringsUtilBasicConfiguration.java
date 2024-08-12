444b81df9df40aad6a22e41659f01dd3c08ade47
Justin Cranford <justincranford@hotmail.com>
2024-08-12 01:08:46 -0400

    Remove unnecessary ComponentScan

--- springs-util-basic/src/main/java/com/github/justincranford/springs/util/basic/config/SpringsUtilBasicConfiguration.java
+++ springs-util-basic/src/main/java/com/github/justincranford/springs/util/basic/config/SpringsUtilBasicConfiguration.java
@@ -1,10 +1,9 @@
 package com.github.justincranford.springs.util.basic.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages={"com.github.justincranford.springs.util.basic"})
+//@ComponentScan(basePackages={"com.github.justincranford.springs.util.basic.util"})
 public class SpringsUtilBasicConfiguration {
 	// do nothing
 }
