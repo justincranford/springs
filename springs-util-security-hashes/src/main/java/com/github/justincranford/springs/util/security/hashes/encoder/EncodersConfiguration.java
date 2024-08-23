87a9dfed2e3592192e8959b764e10a9f9b81cd4d
Justin Cranford <justincranford@hotmail.com>
2024-08-23 19:17:35 -0400

    Cleanup

--- springs-util-security-hashes/src/main/java/com/github/justincranford/springs/util/security/hashes/encoder/EncodersConfiguration.java
+++ springs-util-security-hashes/src/main/java/com/github/justincranford/springs/util/security/hashes/encoder/EncodersConfiguration.java
@@ -32,42 +32,57 @@ public class EncodersConfiguration {
 
 	@Bean
 	public KeyEncoders keyEncoders(final SpringsUtilSecurityHashesProperties props) {
-		final Map<String, DerivedSalt>  derivedSaltProps  = props.getEncodersArgon2().getDerivedSalt();
-		final Map<String, ConstantSalt> constantSaltProps = props.getEncodersArgon2().getConstantSalt();
+		final Map<String, DerivedSalt>  derivedSaltPropsMap  = props.getEncodersArgon2().getDerivedSalt();
+		final Map<String, ConstantSalt> constantSaltPropsMap = props.getEncodersArgon2().getConstantSalt();
 		final List<String> keyEncoderConfigNames = new ArrayList<>(props.getKeyEncoder().values());
-		Assert.notEmpty(keyEncoderConfigNames, "Key encoder config names must not be empty");
+		Assert.notEmpty(keyEncoderConfigNames, "KeyEncoder config names must not be empty");
 		Collections.reverse(keyEncoderConfigNames);
 
 		final LinkedHashMap<String, KeyEncoder> idToKeyEncoders = new LinkedHashMap<>();
 		for (final String keyEncoderConfigName : keyEncoderConfigNames) {
-			final EncodersArgon2.DerivedSalt  derivedSalt  = derivedSaltProps.get(keyEncoderConfigName);
-			final EncodersArgon2.ConstantSalt constantSalt = constantSaltProps.get(keyEncoderConfigName);
-			Assert.isTrue((derivedSalt != null) ^ (constantSalt != null), "Key encoder must be derivedSalt or constantSalt, not null and not both");
-			if (derivedSalt != null) {
-				idToKeyEncoders.put(derivedSalt.getId(), new KeyEncoder(new Argon2Encoder.DerivedSalt(derivedSalt.getDerivedSaltLength(), derivedSalt.getAssociatedData().getBytes(), derivedSalt.getHashLength(), derivedSalt.getParallelism(), derivedSalt.getMemoryInKB(), derivedSalt.getIterations())));
-			} else if (constantSalt != null) {
-				idToKeyEncoders.put(constantSalt.getId(), new KeyEncoder(new Argon2Encoder.ConstantSalt(constantSalt.getConstantSalt().getBytes(), constantSalt.getAssociatedData().getBytes(), constantSalt.getHashLength(), constantSalt.getParallelism(), constantSalt.getMemoryInKB(), constantSalt.getIterations())));
+			final boolean isDerivedSalt  = derivedSaltPropsMap.containsKey(keyEncoderConfigName);
+			final boolean isConstantSalt = constantSaltPropsMap.containsKey(keyEncoderConfigName);
+			Assert.isTrue(isDerivedSalt ^ isConstantSalt, "KeyEncoder must be DerivedSalt or ConstantSalt");
+			if (isDerivedSalt) {
+				final EncodersArgon2.DerivedSalt derivedSaltProps = derivedSaltPropsMap.get(keyEncoderConfigName);
+				idToKeyEncoders.put(derivedSaltProps.getId(), buildKeyEncoder(derivedSaltProps));
+			} else if (isConstantSalt) {
+				final EncodersArgon2.ConstantSalt constantSaltProps = constantSaltPropsMap.get(keyEncoderConfigName);
+				idToKeyEncoders.put(constantSaltProps.getId(), buildKeyEncoder(constantSaltProps));
 			}
 		}
-		Assert.notEmpty(idToKeyEncoders, "Key encoders map must not be empty");
+		Assert.notEmpty(idToKeyEncoders, "KeyEncoders map must not be empty");
 		return new KeyEncoders(idToKeyEncoders.firstEntry().getKey(), idToKeyEncoders);
 	}
 
 	@SuppressWarnings("null")
 	@Bean
 	public ValueEncoders valueEncoders(final SpringsUtilSecurityHashesProperties props) {
-		final Map<String, RandomSalt> randomSaltProps = props.getEncodersArgon2().getRandomSalt();
+		final Map<String, RandomSalt> randomSaltPropsMap = props.getEncodersArgon2().getRandomSalt();
 		final List<String> valueEncoderConfigNames = new ArrayList<>(props.getValueEncoder().values());
-		Assert.notEmpty(valueEncoderConfigNames, "Value encoder config names must not be empty");
+		Assert.notEmpty(valueEncoderConfigNames, "ValueEncoder config names must not be empty");
 		Collections.reverse(valueEncoderConfigNames);
 
 		final LinkedHashMap<String, ValueEncoder> idToValueEncoders = new LinkedHashMap<>();
 		for (final String valueEncoderConfigName : valueEncoderConfigNames) {
-			final EncodersArgon2.RandomSalt randomSalt = randomSaltProps.get(valueEncoderConfigName);
-			Assert.isTrue((randomSalt != null), "Value encoder must be randomSalt, not null");
-			idToValueEncoders.put(randomSalt.getId(), new ValueEncoder(new Argon2Encoder.RandomSalt(randomSalt.getRandomSaltLength(), randomSalt.getAssociatedData().getBytes(), randomSalt.getHashLength(), randomSalt.getParallelism(), randomSalt.getMemoryInKB(), randomSalt.getIterations())));
+			final boolean isRandomSalt = randomSaltPropsMap.containsKey(valueEncoderConfigName);
+			Assert.isTrue(isRandomSalt, "ValueEncoder must be RandomSalt");
+			final EncodersArgon2.RandomSalt randomSalt = randomSaltPropsMap.get(valueEncoderConfigName);
+			idToValueEncoders.put(randomSalt.getId(), buildValueEncoder(randomSalt));
 		}
-		Assert.notEmpty(idToValueEncoders, "Key encoders map must not be empty");
+		Assert.notEmpty(idToValueEncoders, "ValueEncoders map must not be empty");
 		return new ValueEncoders(idToValueEncoders.firstEntry().getKey(), idToValueEncoders);
+	}
+
+	private KeyEncoder buildKeyEncoder(final EncodersArgon2.DerivedSalt derivedSalt) {
+		return new KeyEncoder(new Argon2Encoder.DerivedSalt(derivedSalt.getDerivedSaltLength(), derivedSalt.getAssociatedData().getBytes(), derivedSalt.getHashLength(), derivedSalt.getParallelism(), derivedSalt.getMemoryInKB(), derivedSalt.getIterations()));
+	}
+
+	private KeyEncoder buildKeyEncoder(final EncodersArgon2.ConstantSalt constantSalt) {
+		return new KeyEncoder(new Argon2Encoder.ConstantSalt(constantSalt.getConstantSalt().getBytes(), constantSalt.getAssociatedData().getBytes(), constantSalt.getHashLength(), constantSalt.getParallelism(), constantSalt.getMemoryInKB(), constantSalt.getIterations()));
+	}
+
+	private ValueEncoder buildValueEncoder(final EncodersArgon2.RandomSalt randomSalt) {
+		return new ValueEncoder(new Argon2Encoder.RandomSalt(randomSalt.getRandomSaltLength(), randomSalt.getAssociatedData().getBytes(), randomSalt.getHashLength(), randomSalt.getParallelism(), randomSalt.getMemoryInKB(), randomSalt.getIterations()));
 	}
 }
