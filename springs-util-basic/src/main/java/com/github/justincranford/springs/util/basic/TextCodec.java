a1c2e73fd1d45ce32d34dce4212a0c12602416bd
Justin Cranford <justincranford@hotmail.com>
2024-09-21 17:20:03 -0400

    TextCodec

---
+++ springs-util-basic/src/main/java/com/github/justincranford/springs/util/basic/TextCodec.java
@@ -0,0 +1,138 @@
+package com.github.justincranford.springs.util.basic;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Objects;
+
+@SuppressWarnings({"nls"})
+public interface TextEncDec {
+	public static final HexEncDec HEX_UC_STRICT  = new HexEncDec(true,  true,  "HexUpperCaseStrict");
+	public static final HexEncDec HEX_LC_STRICT  = new HexEncDec(false, true,  "HexLowerCaseStrict");
+	public static final HexEncDec HEX_UC_LENIENT = new HexEncDec(true,  false, "HexUpperCaseLenient");
+	public static final HexEncDec HEX_LC_LENIENT = new HexEncDec(false, false, "HexLowerCaseLenient");
+	public static final Base64EncDec BASE64_STD    = new Base64EncDec(Constants.STD_ENCODER,    Constants.STD_DECODER,  "Base64");
+	public static final Base64EncDec BASE64_URL    = new Base64EncDec(Constants.URL_ENCODER,    Constants.URL_DECODER,  "Base64URL");
+	public static final Base64EncDec BASE64_MIME32 = new Base64EncDec(Constants.MIME32_ENCODER, Constants.MIME_DECODER, "Base64Mime32");
+	public static final Base64EncDec BASE64_MIME64 = new Base64EncDec(Constants.MIME64_ENCODER, Constants.MIME_DECODER, "Base64Mime64");
+	public static final Base64EncDec BASE64_MIME76 = new Base64EncDec(Constants.MIME76_ENCODER, Constants.MIME_DECODER, "Base64Mime76");
+	public static final Base64EncDec BASE64_MIME   = new Base64EncDec(Constants.MIME_ENCODER,   Constants.MIME_DECODER, "Base64Mime");
+
+	public byte[] canonicalIdBytes();
+    public String encodeToString(final byte[] bytes);
+    public byte[] encodeToBytes(final byte[] bytes);
+    public byte[] decodeFromString(final String string);
+    public byte[] decodeFromBytes(final byte[] bytes);
+
+	public static class HexEncDec implements TextEncDec {
+		private final byte[]  utf8HexBytes;
+		private final boolean encodeUpperCase;
+		private final boolean decodeStrict;
+		private final byte[]  canonicalIdBytes;
+    	/*package*/ HexEncDec(final boolean encodeUpperCase0, final boolean decodeStrict0, final String canonicalId) {
+    		this.utf8HexBytes     = encodeUpperCase0 ? Constants.HEX_UC_BYTES : Constants.HEX_LC_BYTES;
+    		this.encodeUpperCase  = encodeUpperCase0;
+    		this.decodeStrict     = decodeStrict0;
+    		this.canonicalIdBytes = canonicalId.getBytes(StandardCharsets.UTF_8);
+    	}
+    	@Override
+		public byte[] canonicalIdBytes() {
+			return this.canonicalIdBytes;
+		}
+    	@Override
+        public String encodeToString(final byte[] bytes) {
+    		final byte[] hex = encodeToBytes(Objects.requireNonNull(bytes, "Bytes must be non-null"));
+			return new String(hex, 0, hex.length, StandardCharsets.UTF_8);
+    	}
+    	@Override
+        public byte[] encodeToBytes(final byte[] bytes) {
+    		final byte[] encodedBytes = new byte[Objects.requireNonNull(bytes, "Bytes must be non-null").length * 2];
+	        for (int i = 0, o = 0; i < bytes.length; ) {
+	        	final int b = bytes[i++] & 0xFF;
+	        	encodedBytes[o++] = this.utf8HexBytes[b >>> 4];
+	        	encodedBytes[o++] = this.utf8HexBytes[b & 0x0F];
+	        }
+    		return encodedBytes;
+    	}
+    	@Override
+        public byte[] decodeFromString(final String string) {
+    		return decodeFromBytes(Objects.requireNonNull(string, "String must be non-null").getBytes(StandardCharsets.UTF_8));
+    	}
+    	@Override
+        public byte[] decodeFromBytes(final byte[] bytes) {
+            if (Objects.requireNonNull(bytes, "Bytes must be non-null").length % 2 != 0) {
+                throw new IllegalArgumentException("Input must have even length");
+            }
+            final byte[] decodingBytes = this.decodeStrict ? bytes : this.encodeUpperCase
+            		? new String(bytes, StandardCharsets.UTF_8).toUpperCase().getBytes(StandardCharsets.UTF_8)
+            		: new String(bytes, StandardCharsets.UTF_8).toLowerCase().getBytes(StandardCharsets.UTF_8);
+
+    		final byte[] decodedBytes = new byte[decodingBytes.length / 2];
+            for (int i = 0, o = 0; i < decodingBytes.length; ) {
+                final int highHalfByte = utf8HexCharToByte(decodingBytes[i++]);
+                final int lowNibble  = utf8HexCharToByte(decodingBytes[i++]);
+                decodedBytes[o++] = (byte) ((highHalfByte << 4) | lowNibble);
+            }
+            return decodedBytes;
+        }
+        private int utf8HexCharToByte(final byte character) {
+            for (int i = 0; i < this.utf8HexBytes.length; i++) {
+                if (this.utf8HexBytes[i] == character) {
+                    return i;
+                }
+            }
+            throw new IllegalArgumentException("Invalid character, expected hex character");
+        }
+    }
+
+	public static class Base64EncDec implements TextEncDec {
+    	private final Base64.Encoder encoder;
+    	private final Base64.Decoder decoder;
+    	private final byte[] canonicalIdBytes;
+    	/*package*/ Base64EncDec(final Base64.Encoder encoder0, final Base64.Decoder decoder0, final String canonicalId) {
+    		this.encoder = encoder0;
+    		this.decoder = decoder0;
+    		this.canonicalIdBytes = canonicalId.getBytes(StandardCharsets.UTF_8);
+    	}
+    	@Override
+		public byte[] canonicalIdBytes() {
+			return this.canonicalIdBytes;
+		}
+    	@Override
+        public String encodeToString(final byte[] bytes) {
+    		final byte[] base64 = encodeToBytes(bytes);
+			return new String(base64, 0, base64.length, StandardCharsets.UTF_8);
+    	}
+    	@Override
+        public byte[] encodeToBytes(final byte[] bytes) {
+    		return this.encoder.encode(bytes);
+    	}
+    	@Override
+        public byte[] decodeFromString(final String string) {
+    		return decodeFromBytes(string.getBytes(StandardCharsets.UTF_8));
+    	}
+    	@Override
+        public byte[] decodeFromBytes(final byte[] bytes) {
+    		return this.decoder.decode(bytes);
+    	}
+    }
+
+	public static class Constants {
+		public static final String HEX_UC = "0123456789ABCDEF";
+		public static final String HEX_LC = "0123456789abcdef";
+		public static final byte[] HEX_UC_BYTES = "0123456789ABCDEF".getBytes(StandardCharsets.UTF_8);
+		public static final byte[] HEX_LC_BYTES = "0123456789abcdef".getBytes(StandardCharsets.UTF_8);
+
+		public static final byte[] CRLF = new byte[] {'\r', '\n'};
+		public static final byte[] LF = new byte[] {'\n'};
+
+	    public static final Base64.Encoder STD_ENCODER = Base64.getEncoder().withoutPadding();
+		public static final Base64.Decoder STD_DECODER = Base64.getDecoder();
+		public static final Base64.Encoder MIME32_ENCODER = Base64.getMimeEncoder(32, LF).withoutPadding();
+		public static final Base64.Encoder MIME64_ENCODER = Base64.getMimeEncoder(64, CRLF).withoutPadding();
+		public static final Base64.Encoder MIME76_ENCODER = Base64.getMimeEncoder(76, CRLF).withoutPadding();
+		public static final Base64.Encoder MIME_ENCODER = Base64.getMimeEncoder(Integer.MAX_VALUE, LF).withoutPadding();
+		public static final Base64.Decoder MIME_DECODER = Base64.getMimeDecoder();
+	    public static final Base64.Encoder URL_ENCODER = Base64.getUrlEncoder().withoutPadding();
+		public static final Base64.Decoder URL_DECODER = Base64.getUrlDecoder();
+	}
+}
---
+++ springs-util-basic/src/test/java/com/github/justincranford/springs/util/basic/TextCodecTest.java
@@ -0,0 +1,112 @@
+package com.github.justincranford.springs.util.basic;
+
+import static com.github.justincranford.springs.util.basic.TextEncDec.HEX_LC_LENIENT;
+import static com.github.justincranford.springs.util.basic.TextEncDec.HEX_LC_STRICT;
+import static com.github.justincranford.springs.util.basic.TextEncDec.HEX_UC_LENIENT;
+import static com.github.justincranford.springs.util.basic.TextEncDec.HEX_UC_STRICT;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.justincranford.springs.util.basic.TextEncDec.HexEncDec;
+
+@SuppressWarnings({"nls", "static-method"})
+public class HexEncDecTest {
+    private static final byte[] EMPTY_BYTES = { };
+    private static final String EMPTY_STRING = "";
+    private static final byte[] UNENCODED_BYTES = {0x0A, 0x1B, 0x2C, 0x3D, 0x4E, 0x5F, 0x67, (byte) 0x89};
+    private static final String ENCODED_STRING_MIXED_CASE = "0A1b2C3d4E5f6789";
+    private static final String ENCODED_STRING_UPPER_CASE = ENCODED_STRING_MIXED_CASE.toUpperCase();
+    private static final String ENCODED_STRING_LOWER_CASE = ENCODED_STRING_MIXED_CASE.toLowerCase();
+
+    @Test
+    void testUcStrict() {
+		verifyEncodeSuccess(HEX_UC_STRICT, UNENCODED_BYTES, ENCODED_STRING_UPPER_CASE);
+		verifyDecodeSuccess(HEX_UC_STRICT, ENCODED_STRING_UPPER_CASE, UNENCODED_BYTES);
+		verifyStrictDecodeFail(HEX_UC_STRICT, ENCODED_STRING_LOWER_CASE);
+		verifyStrictDecodeFail(HEX_UC_STRICT, ENCODED_STRING_MIXED_CASE);
+		verifyUnevenFail(HEX_UC_STRICT);
+		verifyNullFail(HEX_UC_STRICT);
+
+		verifyEncodeSuccess(HEX_UC_STRICT, EMPTY_BYTES, EMPTY_STRING);
+		verifyDecodeSuccess(HEX_UC_STRICT, EMPTY_STRING, EMPTY_BYTES);
+    }
+
+    @Test
+    void testUcLenient() {
+		verifyEncodeSuccess(HEX_UC_LENIENT, UNENCODED_BYTES, ENCODED_STRING_UPPER_CASE);
+		verifyDecodeSuccess(HEX_UC_LENIENT, ENCODED_STRING_UPPER_CASE, UNENCODED_BYTES);
+		verifyDecodeSuccess(HEX_UC_LENIENT, ENCODED_STRING_LOWER_CASE, UNENCODED_BYTES);
+		verifyDecodeSuccess(HEX_UC_LENIENT, ENCODED_STRING_MIXED_CASE, UNENCODED_BYTES);
+		verifyUnevenFail(HEX_UC_LENIENT);
+		verifyNullFail(HEX_UC_LENIENT);
+
+		verifyEncodeSuccess(HEX_UC_LENIENT, EMPTY_BYTES, EMPTY_STRING);
+		verifyDecodeSuccess(HEX_UC_LENIENT, EMPTY_STRING, EMPTY_BYTES);
+    }
+
+    @Test
+    void testLcStrict() {
+		verifyEncodeSuccess(HEX_LC_STRICT, UNENCODED_BYTES, ENCODED_STRING_LOWER_CASE);
+		verifyDecodeSuccess(HEX_LC_STRICT, ENCODED_STRING_LOWER_CASE, UNENCODED_BYTES);
+		verifyStrictDecodeFail(HEX_LC_STRICT, ENCODED_STRING_UPPER_CASE);
+		verifyStrictDecodeFail(HEX_LC_STRICT, ENCODED_STRING_MIXED_CASE);
+		verifyUnevenFail(HEX_LC_STRICT);
+		verifyNullFail(HEX_LC_STRICT);
+
+		verifyEncodeSuccess(HEX_LC_STRICT, EMPTY_BYTES, EMPTY_STRING);
+		verifyDecodeSuccess(HEX_LC_STRICT, EMPTY_STRING, EMPTY_BYTES);
+    }
+
+    @Test
+    void testLcLenient() {
+		verifyEncodeSuccess(HEX_LC_LENIENT, UNENCODED_BYTES, ENCODED_STRING_LOWER_CASE);
+		verifyDecodeSuccess(HEX_LC_LENIENT, ENCODED_STRING_LOWER_CASE, UNENCODED_BYTES);
+		verifyDecodeSuccess(HEX_LC_LENIENT, ENCODED_STRING_LOWER_CASE, UNENCODED_BYTES);
+		verifyDecodeSuccess(HEX_LC_LENIENT, ENCODED_STRING_MIXED_CASE, UNENCODED_BYTES);
+		verifyUnevenFail(HEX_LC_LENIENT);
+		verifyNullFail(HEX_LC_LENIENT);
+
+		verifyEncodeSuccess(HEX_LC_LENIENT, EMPTY_BYTES, EMPTY_STRING);
+		verifyDecodeSuccess(HEX_LC_LENIENT, EMPTY_STRING, EMPTY_BYTES);
+    }
+
+	private static void verifyEncodeSuccess(final HexEncDec hexEncDec, final byte[] unencodedBytes, final String encodedString) {
+		assertEquals(encodedString, hexEncDec.encodeToString(unencodedBytes));
+        assertArrayEquals(encodedString.getBytes(StandardCharsets.UTF_8), hexEncDec.encodeToBytes(unencodedBytes));
+    }
+
+	private void verifyDecodeSuccess(final HexEncDec hexEncDec, final String encodedString, final byte[] unencodedBytes) {
+		assertArrayEquals(unencodedBytes, hexEncDec.decodeFromBytes(encodedString.getBytes(StandardCharsets.UTF_8)));
+        assertArrayEquals(unencodedBytes, hexEncDec.decodeFromString(encodedString));
+	}
+
+	private void verifyStrictDecodeFail(final HexEncDec hexEncDec, final String encodedString) {
+		final IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> hexEncDec.decodeFromString(encodedString));
+		assertEquals("Invalid character, expected hex character", exception2.getMessage());
+		final IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> hexEncDec.decodeFromBytes(encodedString.getBytes(StandardCharsets.UTF_8)));
+		assertEquals("Invalid character, expected hex character", exception1.getMessage());
+	}
+
+	private void verifyUnevenFail(final HexEncDec hexEncDec) {
+		final IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> hexEncDec.decodeFromString("0"));
+		assertEquals("Input must have even length", exception2.getMessage());
+		final IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> hexEncDec.decodeFromBytes("0".getBytes(StandardCharsets.UTF_8)));
+		assertEquals("Input must have even length", exception1.getMessage());
+	}
+
+	private void verifyNullFail(final HexEncDec hexEncDec) {
+		final NullPointerException exception1 = assertThrows(NullPointerException.class, () -> hexEncDec.encodeToString(null));
+		assertEquals("Bytes must be non-null", exception1.getMessage());
+		final NullPointerException exception2 = assertThrows(NullPointerException.class, () -> hexEncDec.decodeFromString(null));
+		assertEquals("String must be non-null", exception2.getMessage());
+		final NullPointerException exception3 = assertThrows(NullPointerException.class, () -> hexEncDec.encodeToBytes(null));
+		assertEquals("Bytes must be non-null", exception3.getMessage());
+		final NullPointerException exception4 = assertThrows(NullPointerException.class, () -> hexEncDec.decodeFromBytes(null));
+		assertEquals("Bytes must be non-null", exception4.getMessage());
+	}
+}
