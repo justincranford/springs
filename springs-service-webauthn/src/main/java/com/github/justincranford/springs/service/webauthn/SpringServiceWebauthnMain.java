f8a4f08a2c7df21557d07c25469da8c53ecca4fd
Justin Cranford <justincranford@hotmail.com>
2024-10-18 14:52:49 -0400

    Webauthn main

---
+++ springs-service-webauthn/src/main/java/com/github/justincranford/springs/service/webauthn/SpringServiceWebauthnMain.java
@@ -0,0 +1,18 @@
+package com.github.justincranford.springs.service.webauthn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.github.justincranford.springs.service.webauthn.config.SpringsServiceWebauthnConfiguration;
+import com.github.justincranford.springs.util.certs.server.TomcatTlsInitializer;
+
+@SpringBootApplication
+@Import({SpringsServiceWebauthnConfiguration.class})
+public class SpringServiceWebauthnMain {
+	public static void main(final String[] args) {
+		final SpringApplication springApplication = new SpringApplication(SpringServiceWebauthnMain.class);
+		springApplication.addInitializers(new TomcatTlsInitializer());
+		springApplication.run(args);
+	}
+}
--- springs-service-webauthn/src/main/java/com/github/justincranford/springs/service/webauthn/config/SpringsServiceWebauthnConfiguration.java
+++ springs-service-webauthn/src/main/java/com/github/justincranford/springs/service/webauthn/config/SpringsServiceWebauthnConfiguration.java
@@ -1,10 +1,17 @@
 package com.github.justincranford.springs.service.webauthn.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.github.justincranford.springs.persistenceorm.base.config.SpringsPersistenceOrmBaseConfiguration;
+import com.github.justincranford.springs.service.http.server.HelloWorldController;
 import com.github.justincranford.springs.service.webauthn.actions.config.ActionsConfiguration;
 import com.github.justincranford.springs.service.webauthn.authenticate.config.AuthenticationConfiguration;
 import com.github.justincranford.springs.service.webauthn.credential.config.CredentialConfiguration;
@@ -13,7 +20,14 @@ import com.github.justincranford.springs.service.webauthn.relyingparty.config.Re
 import com.github.justincranford.springs.util.certs.config.SpringsUtilHttpsConfiguration;
 import com.github.justincranford.springs.util.json.config.SpringsUtilJsonConfiguration;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@EnableAutoConfiguration(
+		exclude = {
+			UserDetailsServiceAutoConfiguration.class
+		}
+	)
 @EnableConfigurationProperties
 @Import(value = {
     SpringsPersistenceOrmBaseConfiguration.class,
@@ -23,8 +37,37 @@ import com.github.justincranford.springs.util.json.config.SpringsUtilJsonConfigu
 	ActionsConfiguration.class,
 	CredentialConfiguration.class,
 	RegistrationConfiguration.class,
-	AuthenticationConfiguration.class
+	AuthenticationConfiguration.class,
+	HelloWorldController.class
 })
+@Slf4j
+@SuppressWarnings({"nls", "static-method"})
 public class SpringsServiceWebauthnConfiguration {
-	// do nothing
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        	.authorizeHttpRequests(authz -> authz.anyRequest().permitAll())
+        	.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+	@Bean
+	public String httpBaseUrl(
+		@Value("${server.address}") final String serverAddress,
+		@Value("${server.port}") final long serverPort
+	) {
+		final String httpBaseUrl = "http://"  + serverAddress + ":" + serverPort;
+		log.info("httpBaseUrl: {}", httpBaseUrl);
+		return httpBaseUrl;
+	}
+
+	@Bean
+	public String httpsBaseUrl(
+		@Value("${server.address}") final String serverAddress,
+		@Value("${server.port}") final long serverPort
+	) {
+		final String httpsBaseUrl = "https://"  + serverAddress + ":" + serverPort;
+		log.info("httpsBaseUrl: {}", httpsBaseUrl);
+		return httpsBaseUrl;
+	}
 }
