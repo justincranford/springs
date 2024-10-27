4f791964973cde1c4536d2023e0a9c3fd292376f
Justin Cranford <justincranford@hotmail.com>
2024-10-27 04:14:02 -0400

    Queries

--- springs-persistence-orm-users/src/main/java/com/github/justincranford/springs/persistenceorm/users/person/PersonaOrmRepository.java
+++ springs-persistence-orm-users/src/main/java/com/github/justincranford/springs/persistenceorm/users/person/PersonaOrmRepository.java
@@ -1,8 +1,16 @@
 package com.github.justincranford.springs.persistenceorm.users.person;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.history.RevisionRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PersonaOrmRepository extends ListCrudRepository<PersonaOrm, Long>, RevisionRepository<PersonaOrm, Long, Long> {
-	// empty
+    @Query("SELECT p FROM PersonaOrm po JOIN po.person p JOIN po.emailAddresses ea WHERE ea.emailAddress = :emailAddress")
+    Optional<PersonOrm> findPersonByEmailAddress(@Param("emailAddress") String emailAddress);
+
+    @Query("SELECT p.password FROM PersonaOrm po JOIN po.person p JOIN po.emailAddresses ea WHERE ea.emailAddress = :emailAddress")
+    Optional<Password> findPasswordByEmailAddress(@Param("emailAddress") String emailAddress);
 }
