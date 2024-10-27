d13b6aff6a8e3d2021b3a6a41f74c4d7172574fe
Justin Cranford <justincranford@hotmail.com>
2024-10-27 04:14:44 -0400

    Unused suppress

--- springs-persistence-orm-users/src/main/java/com/github/justincranford/springs/persistenceorm/users/person/PersonOrm.java
+++ springs-persistence-orm-users/src/main/java/com/github/justincranford/springs/persistenceorm/users/person/PersonOrm.java
@@ -55,7 +55,6 @@ import lombok.experimental.Accessors;
 @SQLDelete(sql="UPDATE person SET pre_delete_date_time=NOW() WHERE id=? AND version=?")
 @SQLRestriction(AbstractEntity.WHERE_CLAUSE)
 @SequenceGenerator(sequenceName="person_sequence",name=AbstractEntity.SEQUENCE_ID,initialValue=AbstractEntity.SEQUENCE_ID_INITIAL_VALUE,allocationSize=AbstractEntity.SEQUENCE_ID_ALLOCATION_SIZE_MEDIUM)
-@SuppressWarnings({"static-method"})
 public class PersonOrm extends AbstractEntity {
     @Column(length=64,nullable=false,unique=true)
 	@Size(min=8,max=64)
