237e46e617cc0b09c550a4530b30cfd76d4ff15e
Justin Cranford <justincranford@hotmail.com>
2024-11-07 00:01:22 -0500

    Refactor IT

--- springs-authentication-orm-users/src/test/java/com/github/justincranford/springs/authenticationorm/users/session/SessionPojoRepositoryIT.java
+++ springs-authentication-orm-users/src/test/java/com/github/justincranford/springs/authenticationorm/users/session/SessionPojoRepositoryIT.java
@@ -1,22 +1,17 @@
 package com.github.justincranford.springs.authenticationorm.users.session;
 
+import static com.github.justincranford.springs.authenticationorm.users.session.RandomPersonUtil.generatePerson;
+import static com.github.justincranford.springs.authenticationorm.users.session.RandomPersonaUtil.generatePersona;
 import static com.github.justincranford.springs.util.basic.SecureRandomUtil.SECURE_RANDOM;
-import static com.github.justincranford.springs.util.basic.SecureRandomUtil.randomEmailAddress;
-import static com.github.justincranford.springs.util.basic.SecureRandomUtil.randomEnumElement;
-import static com.github.justincranford.springs.util.basic.SecureRandomUtil.randomString;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.TimeZone;
 import java.util.UUID;
 import java.util.function.Supplier;
-import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -32,29 +27,9 @@ import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.github.justincranford.springs.authenticationorm.users.AbstractIT;
-import com.github.justincranford.springs.persistenceorm.users.person.LanguageOrm;
-import com.github.justincranford.springs.persistenceorm.users.person.NameOrm;
-import com.github.justincranford.springs.persistenceorm.users.person.PasswordOrm;
 import com.github.justincranford.springs.persistenceorm.users.person.PersonOrm;
-import com.github.justincranford.springs.persistenceorm.users.person.enums.I18nLanguageType;
-import com.github.justincranford.springs.persistenceorm.users.person.enums.L10nRegionType;
-import com.github.justincranford.springs.persistenceorm.users.person.enums.PersonStatusType;
-import com.github.justincranford.springs.persistenceorm.users.persona.EmailAddressOrm;
-import com.github.justincranford.springs.persistenceorm.users.persona.EmailAddressRfc5321Orm;
-import com.github.justincranford.springs.persistenceorm.users.persona.LocationAddressOrm;
 import com.github.justincranford.springs.persistenceorm.users.persona.PersonaOrm;
-import com.github.justincranford.springs.persistenceorm.users.persona.PersonaOrm.PersonaOrmBuilder;
-import com.github.justincranford.springs.persistenceorm.users.persona.PhoneNumberOrm;
-import com.github.justincranford.springs.persistenceorm.users.persona.UrlOrm;
-import com.github.justincranford.springs.persistenceorm.users.persona.enums.EmailAddressType;
-import com.github.justincranford.springs.persistenceorm.users.persona.enums.LocationAddressType;
-import com.github.justincranford.springs.persistenceorm.users.persona.enums.PersonaType;
-import com.github.justincranford.springs.persistenceorm.users.persona.enums.PhoneNumberType;
-import com.github.justincranford.springs.persistenceorm.users.persona.enums.URLType;
-import com.github.justincranford.springs.util.basic.Base64Util;
-import com.github.justincranford.springs.util.basic.SecureRandomUtil;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 
 @Transactional
@@ -75,104 +50,10 @@ public class SessionPojoRepositoryIT extends AbstractIT {
 		this.changedContext = SecurityContextHolder.createEmptyContext();
 		this.changedContext.setAuthentication(new UsernamePasswordAuthenticationToken("changedContext-" + UUID.randomUUID(), "na", AuthorityUtils.createAuthorityList("ROLE_USER")));
 
-		this.personOrm = generatePerson();
-		super.personOrmRepository().save(this.personOrm);
-
+		this.personOrm = super.personOrmRepository().save(generatePerson());
 		if (SECURE_RANDOM.nextBoolean()) {
-			this.personaOrm = generatePersona(this.personOrm);
-			super.personaOrmRepository().save(this.personaOrm);
+			this.personaOrm = super.personaOrmRepository().save(generatePersona(this.personOrm));
 		}
-	}
-
-	public static PersonOrm generatePerson() {
-		return PersonOrm.builder()
-			.username("username-" + randomString(Base64Util.URL, 32))
-			.password(PasswordOrm.builder().password("password" + randomString(Base64Util.URL, 32)).build())
-			.name(NameOrm.builder()
-				.first("First "   + randomString(Base64Util.URL, 32))
-				.middle("Middle " + randomString(Base64Util.URL, 32))
-				.last("Last "     + randomString(Base64Util.URL, 32))
-				.build())
-			.dateOfBirth(LocalDate.ofYearDay(SECURE_RANDOM.nextInt(100) + 1923, SECURE_RANDOM.nextInt(365) + 1))
-			.status(SecureRandomUtil.randomEnumElement(PersonStatusType.class))
-			.languages(List.of(LanguageOrm.builder().i18n(SecureRandomUtil.randomEnumElement(I18nLanguageType.class)).l10n(SecureRandomUtil.randomEnumElement(L10nRegionType.class)).build()))
-			.timezones(List.of(TimeZone.getTimeZone("Americas/Toronto").toString()))
-			.build();
-	}
-
-	public static PersonaOrm generatePersona(@NotNull final PersonOrm personOrm) {
-		final PersonaOrmBuilder personaBuilder = PersonaOrm.builder();
-		if (SECURE_RANDOM.nextBoolean()) {
-			final int rank = SECURE_RANDOM.nextInt(5);
-			final List<EmailAddressOrm> emailAddresses = new ArrayList<>(rank);
-			for (int emailAddressIndex : IntStream.rangeClosed(1, rank).boxed().toList()) {
-				emailAddresses.add(
-					EmailAddressOrm.builder()
-						.rank(emailAddressIndex)
-						.emailAddress(EmailAddressRfc5321Orm.builder().emailAddress(randomEmailAddress()).build())
-						.type(SecureRandomUtil.randomEnumElement(EmailAddressType.class))
-						.build()
-				);
-			}
-			personaBuilder.emailAddresses(emailAddresses);
-		}
-		if (SECURE_RANDOM.nextBoolean()) {
-			final int numPhoneNumbers = SECURE_RANDOM.nextInt(5);
-			final List<PhoneNumberOrm> phoneNumbers = new ArrayList<>(numPhoneNumbers);
-			for (int rank : IntStream.rangeClosed(1, numPhoneNumbers).boxed().toList()) {
-				phoneNumbers.add(
-					PhoneNumberOrm.builder()
-						.rank(rank)
-						.phoneNumber("+1" + SECURE_RANDOM.nextLong(1_000_000_000L, 9_999_999_999L))
-						.talk(SECURE_RANDOM.nextBoolean())
-						.text(SECURE_RANDOM.nextBoolean())
-						.data(SECURE_RANDOM.nextBoolean())
-						.type(randomEnumElement(PhoneNumberType.class))
-						.build()
-					);
-			}
-			personaBuilder.phoneNumbers(phoneNumbers);
-		}
-		if (SECURE_RANDOM.nextBoolean()) {
-			final int numLocationAddresses = SECURE_RANDOM.nextInt(5);
-			final List<LocationAddressOrm> locationAddresses = new ArrayList<>(numLocationAddresses);
-			for (int rank : IntStream.rangeClosed(1, numLocationAddresses).boxed().toList()) {
-				locationAddresses.add(
-					LocationAddressOrm.builder()
-						.rank(rank)
-						.street1(SECURE_RANDOM.nextInt() + " Street Ave")
-						.street2(SECURE_RANDOM.nextBoolean() ? null : "Apartment " + SECURE_RANDOM.nextInt())
-						.city("Ottawa")
-						.state("Ontario")
-						.country("Canada")
-						.type(SecureRandomUtil.randomEnumElement(LocationAddressType.class))
-						.build()
-				);
-			}
-			personaBuilder.locationAddresses(locationAddresses);
-		}
-		if (SECURE_RANDOM.nextBoolean()) {
-			final int numUrls = SECURE_RANDOM.nextInt(5);
-			final List<UrlOrm> urls = new ArrayList<>(numUrls);
-			for (int rank : IntStream.rangeClosed(1, numUrls).boxed().toList()) {
-				urls.add(
-					UrlOrm.builder()
-						.rank(rank)
-						.url("https://example.com")
-						.type(randomEnumElement(URLType.class))
-						.build()
-				);
-			}
-			personaBuilder.urls(urls);
-		}
-		if (SECURE_RANDOM.nextBoolean()) {
-			personaBuilder.personaType(randomEnumElement(PersonaType.class));
-		}
-		personaBuilder.person(personOrm);
-
-		final PersonaOrm personaOrm = personaBuilder.build();
-		personOrm.personas().add(personaOrm);
-		return personaOrm;
 	}
 
 	private SessionPojo createSession() {
