5185e661abaca948ffa6aef09b79ac2248178e4f
Justin Cranford <justincranford@hotmail.com>
2024-11-11 21:57:20 -0500

    Do successful UI login using random person from properties

--- springs-authentication-orm-users/src/test/java/com/github/justincranford/springs/authenticationorm/users/authentication/HttpsUiAuthenticationIT.java
+++ springs-authentication-orm-users/src/test/java/com/github/justincranford/springs/authenticationorm/users/authentication/HttpsUiAuthenticationIT.java
@@ -3,6 +3,10 @@ package com.github.justincranford.springs.authenticationorm.users.authentication
 import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
+import javax.net.ssl.SSLContext;
+
 import org.htmlunit.WebClient;
 import org.htmlunit.html.HtmlButton;
 import org.htmlunit.html.HtmlForm;
@@ -15,7 +19,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import com.github.justincranford.springs.authenticationorm.users.AbstractIT;
+import com.github.justincranford.springs.persistenceorm.users.person.PersonOrm;
+import com.github.justincranford.springs.persistenceorm.users.properties.SpringsPersistenceOrmUsersPeopleProperties;
 import com.github.justincranford.springs.service.http.client.RestTemplateUtil;
+import com.github.justincranford.springs.util.basic.SecureRandomUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,23 +30,25 @@ import lombok.extern.slf4j.Slf4j;
 @SuppressWarnings({"nls"})
 public class HttpsUiAuthenticationIT extends AbstractIT {
 	@Test
-	void testHttpsLoginSuccess_personPassword_serverTls() throws Exception {
-		final String response = RestTemplateUtil.plainGet(stlsRestTemplate(), httpsBaseUrl() + "/login", String.class);
+	void testHttpsLoginRedirect_whenUnauthenticated() throws Exception {
+		final String response = RestTemplateUtil.plainGet(stlsRestTemplate(), httpsBaseUrl() + "/secure/home", String.class);
 		assertThat(response).contains("action=\"/login\"");
+	}
 
-		attemptLogin("admin1","admin1password");
+	@Test
+	void testHttpsLoginSuccess_personPassword_serverTls() throws Exception {
+		final SpringsPersistenceOrmUsersPeopleProperties.Person person = SecureRandomUtil.randomListElement(springsPersistenceOrmUsersPeopleProperties().getPeople());
+		attemptLogin(stlsSslContext(), person.getUsername(), person.getPassword()); // clear password from properties
+	}
+
+	@Test
+	void testHttpsLoginSuccess_personPassword_mutualTls() throws Exception {
+		final SpringsPersistenceOrmUsersPeopleProperties.Person person = SecureRandomUtil.randomListElement(springsPersistenceOrmUsersPeopleProperties().getPeople());
+		attemptLogin(mtlsSslContext(), person.getUsername(), person.getPassword()); // clear password from properties
 	}
 
 //	@Test
-//	void testHttpsLoginSuccess_personPassword_mutualTls() throws Exception {
-//		final String response = RestTemplateUtil.plainGet(mtlsRestTemplate(), httpsBaseUrl() + "/login", String.class);
-//		assertThat(response).contains("action=\"/login\"");
-//
-//		attemptLogin("admin1","admin1password");
-//	}
-//
-//	@Test
-//	void testHttps_unauthenticated() {
+//	void testHttps_unauthenticated() throws Exception {
 //		final AbstractThrowableAssert<?, ? extends Throwable> assertThatThrownBy = assertThatThrownBy(
 //			() -> RestTemplateUtil.plainGet(httpRestTemplate(), httpsBaseUrl() + "/secure/home", String.class)
 //		);
@@ -49,6 +58,9 @@ public class HttpsUiAuthenticationIT extends AbstractIT {
 //			.cause()
 //			.isInstanceOf(NoHttpResponseException.class)
 //			.hasMessage("localhost:8443 failed to respond");
+//
+//		final PersonOrm personOrm = SecureRandomUtil.randomListElement(personOrmRepository().findAll());
+//		attemptLogin(stlsSslContext(), personOrm.username(), personOrm.password().password()); // hashes password from database
 //	}
 
 	//<form class="form-signin" method="post" action="/login">
@@ -56,10 +68,9 @@ public class HttpsUiAuthenticationIT extends AbstractIT {
 	// <input type="password" id="password" name="password" class="form-control" placeholder="Password" required=""/>
 	// <input name="_csrf" type="hidden" value="JmqZF4H33AOhG1U5tBnEWzQWH0Zt7OI8UOIyqK7505Koa8ZRFlr7LuKV62eMKGQJ0TTwa1YjMiQPjtERMYELkcjPtqGeCv9h"/>
 	// <button class="btn btn-lg btn-primary btn-block" type="submit">
-	private void attemptLogin(final String username, final String password) throws Exception {
+	private void attemptLogin(final SSLContext sslContext, final String username, final String password) throws Exception {
 		try (final WebClient webClient = new WebClient()) {
-//			webClient.getOptions().setSSLContext(serverTlsSslContext);
-			webClient.getOptions().setUseInsecureSSL(true);
+			webClient.getOptions().setSSLContext(sslContext);
 			webClient.getOptions().setCssEnabled(false);
 			webClient.getOptions().setJavaScriptEnabled(false);
 
