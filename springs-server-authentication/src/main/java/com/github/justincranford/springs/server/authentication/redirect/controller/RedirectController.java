9064dcdc9c8e4a937875fed91a0a44e894551b99
Justin Cranford <justincranford@hotmail.com>
2024-11-25 03:11:34 -0500

    Comments

--- springs-server-authentication/src/main/java/com/github/justincranford/springs/server/authentication/redirect/controller/RedirectController.java
+++ springs-server-authentication/src/main/java/com/github/justincranford/springs/server/authentication/redirect/controller/RedirectController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @SuppressWarnings({ "static-method" })
 public class RedirectController {
-//	@GetMapping("/")
+//	@GetMapping({"/", "/index.html", "/home.html", "/login"})
 //	public String redirectToLogin() {
 //		return "redirect:/login";
 //	}
