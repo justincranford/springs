9259276872ac870d26f596aa176f3a94effa7dea
Justin Cranford <justincranford@hotmail.com>
2024-11-25 09:57:54 -0500

    Rename var

--- springs-server-authentication/src/test/java/com/github/justincranford/springs/server/authentication/filterchain/SpringsServerAuthenticationSecurityFilterChainConfigurationIT.java
+++ springs-server-authentication/src/test/java/com/github/justincranford/springs/server/authentication/filterchain/SpringsServerAuthenticationSecurityFilterChainConfigurationIT.java
@@ -10,36 +10,36 @@ import org.springframework.web.client.HttpClientErrorException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SpringsServerAuthenticationSecurityFilterChainConfigurationIT extends AbstractIT {
-	private static final String AUTHORIZE = null;
+	private static final String NO_AUTHORIZE = null;
 
 	@Test
     void testRoot_notAuthenticated() {
-		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/", AUTHORIZE, String.class);
+		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/", NO_AUTHORIZE, String.class);
 	}
 
     @Test
     void testIndexHtml_notAuthenticated() {
-		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/index.html", AUTHORIZE, String.class);
+		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/index.html", NO_AUTHORIZE, String.class);
 	}
 
     @Test
     void testHomeHtml_notAuthenticated() {
-		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/home.html", AUTHORIZE, String.class);
+		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/home.html", NO_AUTHORIZE, String.class);
 	}
 
     @Test
     void testLogin_notAuthenticated() {
-		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/login", AUTHORIZE, String.class);
+		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/login", NO_AUTHORIZE, String.class);
 	}
 
     @Test
     void testLogout_notAuthenticated() {
-		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/logout", AUTHORIZE, String.class);
+		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/logout", NO_AUTHORIZE, String.class);
 	}
 
     @Test
     void testHelloWorld_notAuthenticated() {
-		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/helloworld", AUTHORIZE, String.class);
+		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/helloworld", NO_AUTHORIZE, String.class);
 	}
 
     @Test
@@ -49,18 +49,18 @@ public class SpringsServerAuthenticationSecurityFilterChainConfigurationIT exten
 
     @Test
     void testSecureNotFound_notAuthenticated() {
-		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/secure/notfound", AUTHORIZE, String.class);
+		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/secure/notfound", NO_AUTHORIZE, String.class);
     }
 
     @Disabled
     @Test
     void testSecureNotFound_authenticated() {
-		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/secure/notfound", AUTHORIZE, String.class);
+		RestTemplateUtil.anyGet(stlsRestTemplate(), httpsBaseUrl() + "/secure/notfound", NO_AUTHORIZE, String.class);
     }
 
 	private void verifyNotFound(final String url) {
 		final AbstractThrowableAssert<?, ? extends Throwable> assertThatThrownBy = assertThatThrownBy(
-			() -> RestTemplateUtil.anyGet(stlsRestTemplate(), url, AUTHORIZE, String.class)
+			() -> RestTemplateUtil.anyGet(stlsRestTemplate(), url, NO_AUTHORIZE, String.class)
 		);
 		assertThatThrownBy
 			.isInstanceOf(RuntimeException.class)
