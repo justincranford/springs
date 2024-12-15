77ededa9da6f6684f6e98460f4b5b00fcbfac52d
Justin Cranford <justincranford@hotmail.com>
2024-12-14 23:21:55 -0500

    Checkpoint

--- springs-persistence-redis-session/src/main/java/com/github/justincranford/springs/persistenceredis/serdes/serdes/JsonRedisSerializer.java
+++ springs-persistence-redis-session/src/main/java/com/github/justincranford/springs/persistenceredis/serdes/serdes/JsonRedisSerializer.java
@@ -1,14 +1,34 @@
 package com.github.justincranford.springs.persistenceredis.serdes.serdes;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
 import org.springframework.stereotype.Component;
 
 @Component
-public final class JsonRedisSerializer extends AbstractJsonRedisSerializer<Object> {
-    public JsonRedisSerializer(
-        @Qualifier("objectMapperPersistence") final ObjectMapper objectMapperPersistence
-    ) {
-        super(objectMapperPersistence, Object.class);
+@RequiredArgsConstructor
+public class JsonRedisSerializer implements RedisSerializer<Object> {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public byte[] serialize(final Object value) throws SerializationException {
+        try {
+            return this.objectMapper.writeValueAsBytes(value);
+        } catch (Exception e) {
+            throw new SerializationException("Error serializing object", e);
+        }
+    }
+
+    @Override
+    public Object deserialize(final byte[] bytes) throws SerializationException {
+        try {
+            if (bytes == null || bytes.length == 0) {
+                return null;
+            }
+            return this.objectMapper.readValue(bytes, Object.class);
+        } catch (Exception e) {
+            throw new SerializationException("Error deserializing object", e);
+        }
     }
 }
