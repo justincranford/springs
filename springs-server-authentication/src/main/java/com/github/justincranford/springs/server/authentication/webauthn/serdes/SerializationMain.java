b50734835c33f6d381ad66709bc2b919ce28d28e
Justin Cranford <justincranford@hotmail.com>
2024-12-17 02:14:18 -0500

    Spring Security Webauthn Mixin hell

--- springs-server-authentication/src/main/java/com/github/justincranford/springs/server/authentication/webauthn/serdes/SerializationMain.java
+++ springs-server-authentication/src/main/java/com/github/justincranford/springs/server/authentication/webauthn/serdes/SerializationMain.java
@@ -11,21 +11,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.security.web.webauthn.api.AttestationConveyancePreference;
 import org.springframework.security.web.webauthn.api.AuthenticationExtensionsClientInput;
 import org.springframework.security.web.webauthn.api.AuthenticationExtensionsClientInputs;
+import org.springframework.security.web.webauthn.api.AuthenticatorAttachment;
+import org.springframework.security.web.webauthn.api.AuthenticatorSelectionCriteria;
 import org.springframework.security.web.webauthn.api.AuthenticatorTransport;
 import org.springframework.security.web.webauthn.api.Bytes;
+import org.springframework.security.web.webauthn.api.COSEAlgorithmIdentifier;
 import org.springframework.security.web.webauthn.api.CredProtectAuthenticationExtensionsClientInput;
 import org.springframework.security.web.webauthn.api.CredProtectAuthenticationExtensionsClientInput.CredProtect;
 import org.springframework.security.web.webauthn.api.CredProtectAuthenticationExtensionsClientInput.CredProtect.ProtectionPolicy;
 import org.springframework.security.web.webauthn.api.ImmutableAuthenticationExtensionsClientInputs;
+import org.springframework.security.web.webauthn.api.ImmutablePublicKeyCredentialUserEntity;
 import org.springframework.security.web.webauthn.api.PublicKeyCredentialCreationOptions;
 import org.springframework.security.web.webauthn.api.PublicKeyCredentialDescriptor;
+import org.springframework.security.web.webauthn.api.PublicKeyCredentialParameters;
 import org.springframework.security.web.webauthn.api.PublicKeyCredentialRequestOptions;
+import org.springframework.security.web.webauthn.api.PublicKeyCredentialRpEntity;
 import org.springframework.security.web.webauthn.api.PublicKeyCredentialType;
+import org.springframework.security.web.webauthn.api.PublicKeyCredentialUserEntity;
+import org.springframework.security.web.webauthn.api.ResidentKeyRequirement;
 import org.springframework.security.web.webauthn.api.UserVerificationRequirement;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -45,25 +55,64 @@ public final class SerializationMain {
             .enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
             ;
 
-        objectMapper.addMixIn(PublicKeyCredentialRequestOptions.class, WebauthnPublicKeyCredentialRequestOptionsMixIn.class);
+        objectMapper.addMixIn(Bytes.class, WebauthnBytesMixIn.class);
 
         objectMapper.addMixIn(PublicKeyCredentialCreationOptions.class, WebauthnPublicKeyCredentialCreationOptionsMixIn.class);
+        objectMapper.addMixIn(ImmutablePublicKeyCredentialUserEntity.class, PublicKeyCredentialUserEntityMixIn.class);
+        objectMapper.addMixIn(PublicKeyCredentialUserEntity.class, PublicKeyCredentialUserEntityMixIn.class);
+        objectMapper.addMixIn(PublicKeyCredentialRpEntity.class, PublicKeyCredentialRpEntityMixIn.class);
+        objectMapper.addMixIn(PublicKeyCredentialParameters.class, PublicKeyCredentialParametersMixIn.class);
+        objectMapper.addMixIn(PublicKeyCredentialType.class, PublicKeyCredentialTypeMixIn.class);
+        objectMapper.addMixIn(COSEAlgorithmIdentifier.class, COSEAlgorithmIdentifierMixIn.class);
+        objectMapper.addMixIn(AuthenticatorSelectionCriteria.class, AuthenticatorSelectionCriteriaMixIn.class);
+        objectMapper.addMixIn(AttestationConveyancePreference.class, AttestationConveyancePreferenceMixIn.class);
+        objectMapper.addMixIn(AuthenticatorAttachment.class, AuthenticatorAttachmentMixIn.class);
+        objectMapper.addMixIn(ResidentKeyRequirement.class, ResidentKeyRequirementMixIn.class);
         objectMapper.addMixIn(UserVerificationRequirement.class, UserVerificationRequirementMixIn.class);
+
+        objectMapper.addMixIn(PublicKeyCredentialRequestOptions.class, WebauthnPublicKeyCredentialRequestOptionsMixIn.class);
         objectMapper.addMixIn(ImmutableAuthenticationExtensionsClientInputs.class, AuthenticationExtensionsClientInputsMixIn.class);
         objectMapper.addMixIn(AuthenticationExtensionsClientInputs.class, AuthenticationExtensionsClientInputsMixIn.class);
         objectMapper.addMixIn(AuthenticationExtensionsClientInput.class, AuthenticationExtensionsClientInputMixIn.class);
         objectMapper.addMixIn(PublicKeyCredentialDescriptor.class, PublicKeyCredentialDescriptorMixIn.class);
-        objectMapper.addMixIn(PublicKeyCredentialType.class, PublicKeyCredentialTypeMixIn.class);
         objectMapper.addMixIn(AuthenticatorTransport.class, AuthenticatorTransportMixIn.class);
         objectMapper.addMixIn(CredProtectAuthenticationExtensionsClientInput.class, CredProtectAuthenticationExtensionsClientInputMixIn.class);
         objectMapper.addMixIn(CredProtect.class, CredProtectMixIn.class);
-        objectMapper.addMixIn(Bytes.class, WebauthnBytesMixIn.class);
 
         final Bytes bytes = new Bytes(new byte[] {1, 2, 3, 4, 5, 6 });
         final String serializedBytes = objectMapper.writeValueAsString(bytes);
         System.out.println("Serialized  Bytes: " + serializedBytes);
         final Bytes deserializedBytes = objectMapper.readValue(serializedBytes, Bytes.class);
         System.out.println("Deserialized Bytes: " + deserializedBytes + "\n");
+
+        final PublicKeyCredentialCreationOptions publicKeyCredentialCreationOptions = PublicKeyCredentialCreationOptions.builder()
+            .rp(PublicKeyCredentialRpEntity.builder().id("example.com").name("Example RP").build())
+            .user(ImmutablePublicKeyCredentialUserEntity.builder().name("name").id(Bytes.random()).displayName("displayName").build())
+            .challenge(Bytes.random())
+            .pubKeyCredParams(List.of(PublicKeyCredentialParameters.ES384, PublicKeyCredentialParameters.EdDSA, PublicKeyCredentialParameters.RS512))
+            .timeout(Duration.ofSeconds(60))
+            .excludeCredentials(Collections.singletonList(
+                PublicKeyCredentialDescriptor.builder()
+                    .id(Bytes.random())
+                    .type(PublicKeyCredentialType.PUBLIC_KEY)
+                    .transports(Set.of(AuthenticatorTransport.INTERNAL, AuthenticatorTransport.HYBRID))
+                    .build()
+            ))
+            .authenticatorSelection(AuthenticatorSelectionCriteria.builder()
+                .userVerification(UserVerificationRequirement.PREFERRED)
+                .residentKey(ResidentKeyRequirement.REQUIRED)
+                .authenticatorAttachment(AuthenticatorAttachment.PLATFORM)
+                .build()
+            )
+            .attestation(AttestationConveyancePreference.DIRECT)
+            .extensions(
+                new ImmutableAuthenticationExtensionsClientInputs(new CredProtectAuthenticationExtensionsClientInput(new CredProtect(ProtectionPolicy.USER_VERIFICATION_REQUIRED, true)))
+            )
+            .build();
+        final String serializedPublicKeyCredentialCreationOptions = objectMapper.writeValueAsString(publicKeyCredentialCreationOptions);
+        System.out.println("Serialized   publicKeyCredentialCreationOptions: " + serializedPublicKeyCredentialCreationOptions);
+        final PublicKeyCredentialCreationOptions deserializedPublicKeyCredentialCreationOptions = objectMapper.readValue(serializedPublicKeyCredentialCreationOptions, PublicKeyCredentialCreationOptions.class);
+        System.out.println("Deserialized publicKeyCredentialCreationOptions: " + deserializedPublicKeyCredentialCreationOptions + "\n");
 
         final PublicKeyCredentialRequestOptions publicKeyCredentialRequestOptions = PublicKeyCredentialRequestOptions.builder()
             .challenge(Bytes.random())
@@ -103,12 +152,84 @@ public final class SerializationMain {
     public static abstract class WebauthnPublicKeyCredentialCreationOptionsMixIn {
         @JsonCreator
         public WebauthnPublicKeyCredentialCreationOptionsMixIn(
+            @JsonProperty("rp") PublicKeyCredentialRpEntity rp,
+            @JsonProperty("user") PublicKeyCredentialUserEntity user,
             @JsonProperty("challenge") Bytes challenge,
+            @JsonProperty("pubKeyCredParams") List<PublicKeyCredentialParameters> pubKeyCredParams,
             @JsonProperty("timeout") Duration timeout,
-            @JsonProperty("rpId") String rpId,
-            @JsonProperty("allowCredentials") List<PublicKeyCredentialDescriptor> allowCredentials,
-            @JsonProperty("userVerification") UserVerificationRequirement userVerification,
+            @JsonProperty("excludeCredentials") List<PublicKeyCredentialDescriptor> excludeCredentials,
+            @JsonProperty("authenticatorSelection") AuthenticatorSelectionCriteria authenticatorSelection,
+            @JsonProperty("attestation") AttestationConveyancePreference attestation,
             @JsonProperty("extensions") AuthenticationExtensionsClientInputs extensions
+        ) { }
+    }
+
+    /** @see org.springframework.security.web.webauthn.api.PublicKeyCredentialRpEntity */
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    public static abstract class PublicKeyCredentialRpEntityMixIn {
+        @JsonCreator
+        public PublicKeyCredentialRpEntityMixIn(
+            @JsonProperty("name") String name,
+            @JsonProperty("id") String id
+        ) { }
+    }
+
+    /** @see org.springframework.security.web.webauthn.api.PublicKeyCredentialUserEntity */
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    public static abstract class PublicKeyCredentialUserEntityMixIn {
+        @JsonCreator
+        public PublicKeyCredentialUserEntityMixIn(
+            @JsonProperty("name") String name,
+            @JsonProperty("id") Bytes id,
+            @JsonProperty("displayName") String displayName
+        ) { }
+    }
+
+    /** @see org.springframework.security.web.webauthn.api.PublicKeyCredentialParameters */
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    public static abstract class PublicKeyCredentialParametersMixIn {
+        @JsonCreator
+        public PublicKeyCredentialParametersMixIn(
+            @JsonProperty("type") PublicKeyCredentialType type,
+            @JsonProperty("alg") COSEAlgorithmIdentifier alg
+        ) { }
+    }
+
+    /** @see org.springframework.security.web.webauthn.api.AuthenticatorSelectionCriteria */
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    public static abstract class AuthenticatorSelectionCriteriaMixIn {
+        @JsonCreator
+        public AuthenticatorSelectionCriteriaMixIn(
+            @JsonProperty("authenticatorAttachment") AuthenticatorAttachment authenticatorAttachment,
+            @JsonProperty("residentKey") ResidentKeyRequirement residentKey,
+            @JsonProperty("userVerification") UserVerificationRequirement userVerification
+        ) { }
+    }
+
+    /** @see org.springframework.security.web.webauthn.api.AttestationConveyancePreference */
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    public static abstract class AttestationConveyancePreferenceMixIn {
+        @JsonCreator
+        public AttestationConveyancePreferenceMixIn(
+            @JsonProperty("value") String value
+        ) { }
+    }
+
+    /** @see org.springframework.security.web.webauthn.api.ResidentKeyRequirement */
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    public static abstract class ResidentKeyRequirementMixIn {
+        @JsonCreator
+        public ResidentKeyRequirementMixIn(
+            @JsonProperty("value") String value
+        ) { }
+    }
+
+    /** @see org.springframework.security.web.webauthn.api.UserVerificationRequirement */
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    public static abstract class UserVerificationRequirementMixIn {
+        @JsonCreator
+        public UserVerificationRequirementMixIn(
+            @JsonProperty("value") String value
         ) { }
     }
 
@@ -126,11 +247,11 @@ public final class SerializationMain {
         ) { }
     }
 
-    /** @see org.springframework.security.web.webauthn.api.UserVerificationRequirement */
+    /** @see org.springframework.security.web.webauthn.api.AuthenticatorAttachment */
     @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
-    public static abstract class UserVerificationRequirementMixIn {
+    public static abstract class AuthenticatorAttachmentMixIn {
         @JsonCreator
-        public UserVerificationRequirementMixIn(
+        public AuthenticatorAttachmentMixIn(
             @JsonProperty("value") String value
         ) { }
     }
@@ -173,6 +294,16 @@ public final class SerializationMain {
             @JsonProperty("value") String value
         ) { }
     }
+
+    /** @see org.springframework.security.web.webauthn.api.COSEAlgorithmIdentifier */
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    public static abstract class COSEAlgorithmIdentifierMixIn {
+        @JsonCreator
+        public COSEAlgorithmIdentifierMixIn(
+            @JsonProperty("value") long value
+        ) { }
+    }
+
 
     /** @see org.springframework.security.web.webauthn.api.AuthenticatorTransport */
     @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
