485b24ebeccb82783c1d344efe225684e76d6257
Justin Cranford <justincranford@hotmail.com>
2024-12-31 00:37:19 -0500

    Add experimental anonymousUser handling in PublicKeyCredentialUserEntityRepository

--- springs-server-authentication/src/main/java/com/github/justincranford/springs/server/authentication/webauthn/user/service/PublicKeyCredentialUserEntityRepositoryService.java
+++ springs-server-authentication/src/main/java/com/github/justincranford/springs/server/authentication/webauthn/user/service/PublicKeyCredentialUserEntityRepositoryService.java
@@ -27,11 +27,25 @@ public class PublicKeyCredentialUserEntityRepositoryService implements PublicKey
     @Autowired
     private PrettyJson prettyJson;
 
+//    private ConcurrentHashMap<byte[], PublicKeyCredentialUserEntity> anonymousUsers = new ConcurrentHashMap<>();
+    private PublicKeyCredentialUserEntity anonymousUser;
+
     @Transactional
     @Override
     public PublicKeyCredentialUserEntity findById(final Bytes userId) {
         try {
             log.debug("Searching for WebAuthn user by userId: {}", userId);
+
+            if ((this.anonymousUser != null) && (MessageDigest.isEqual(this.anonymousUser.getId().getBytes(), userId.getBytes()))) {
+                log.info("Found WebAuthn anonymous user by userId: {}, userEntity: {}", userId, this.prettyJson.pretty(this.anonymousUser));
+                return this.anonymousUser;
+            }
+//            final PublicKeyCredentialUserEntity publicKeyCredentialAnonymousUserEntity = this.anonymousUsers.get(userId.getBytes());
+//            if (publicKeyCredentialAnonymousUserEntity != null) {
+//                log.info("Found WebAuthn anonymous user by userId: {}, userEntity: {}", userId, this.prettyJson.pretty(publicKeyCredentialAnonymousUserEntity));
+//                return publicKeyCredentialAnonymousUserEntity;
+//            }
+
             final PersonOrm personOrm = this.personOrmRepository.findByWebauthnId(userId.getBytes()).orElse(null);
             if (personOrm == null) {
                 log.info("No WebAuthn user found by userId: {}", userId);
@@ -52,8 +66,8 @@ public class PublicKeyCredentialUserEntityRepositoryService implements PublicKey
     public PublicKeyCredentialUserEntity findByUsername(final String usernameMixedCase) {
         try {
             if (usernameMixedCase.equalsIgnoreCase(ANONYMOUS_USER)) {
-                log.info("Search for WebAuthn not supported for username: {}", usernameMixedCase);
-                return null;
+                log.info("Found WebAuthn anonymous user by username: {}, userEntity: {}", usernameMixedCase, this.prettyJson.pretty(this.anonymousUser));
+                return this.anonymousUser;
             }
             log.debug("Searching for WebAuthn user by username: {}", usernameMixedCase);
             final String usernameLowerCase = usernameMixedCase.toLowerCase();
@@ -76,10 +90,11 @@ public class PublicKeyCredentialUserEntityRepositoryService implements PublicKey
     @Override
     public void save(final PublicKeyCredentialUserEntity publicKeyCredentialUserEntity) {
         try {
-            if (publicKeyCredentialUserEntity.getName().equalsIgnoreCase(ANONYMOUS_USER)) {
-                throw new IllegalArgumentException("Saving WebAuthn user not allowed: " + publicKeyCredentialUserEntity.getName());
-            }
             log.debug("Saving WebAuthn user: {}", this.prettyJson.pretty(publicKeyCredentialUserEntity));
+            if (publicKeyCredentialUserEntity.getName().equalsIgnoreCase(ANONYMOUS_USER)) {
+                log.info("Saved WebAuthn anonymous user, userEntity: {}", this.prettyJson.pretty(this.anonymousUser));
+                return;
+            }
             final PublicKeyCredentialUserEntity byUserId   = findById(publicKeyCredentialUserEntity.getId());
             final PublicKeyCredentialUserEntity byUsername = findByUsername(publicKeyCredentialUserEntity.getName());
 
@@ -116,6 +131,10 @@ public class PublicKeyCredentialUserEntityRepositoryService implements PublicKey
 
     @Override
     public void delete(final Bytes userId) {
+        if ((this.anonymousUser != null) && (MessageDigest.isEqual(this.anonymousUser.getId().getBytes(), userId.getBytes()))) {
+            log.info("Delete WebAuthn anonymous user by userId: {}, userEntity: {}", userId, this.prettyJson.pretty(this.anonymousUser));
+            return;
+        }
         log.warn("Delete operation not supported for userId: {}", userId);
         throw new UnsupportedOperationException("Delete by userId not supported at this time"); // TODO Maybe later?
     }
